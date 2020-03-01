906dd1dd6104682ac8554e6e213fc547ae4735a2
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-03-01 19:06:03 +0800

    Test

--- bot.py
+++ bot.py
@@ -140,7 +140,11 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         x = request.get_json(force=True)
+        print(type(x))
+        print(x)
         x = x['Message']
+        print(type(x))
+        print(x)
         compose = ''
         compose += '<u>AWS Simple Email Service</u>\n'
         compose += '<b>Notification Type: </b>' + \
