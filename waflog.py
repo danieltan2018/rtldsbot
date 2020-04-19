dda4270dae3593be8dbea49015af60dc56bc597c
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-04-19 13:38:44 +0800

    Add watch length

--- waflog.py
+++ waflog.py
@@ -2,22 +2,26 @@ import re
 email_regex = re.compile('"args":".*","http')
 
 allusers = set()
+counter = {}
 logs = ['log1.txt', 'log2.txt', 'log3.txt', 'log4.txt', 'log5.txt']
 
 for log in logs:
     with open(log, 'r') as logfile:
         for line in logfile:
             try:
-                email = email_regex.search(line).group()
-                email = email.split(',')[0]
-                email = email.replace('"args":"', '')
-                email = email.strip('"')
-                email = email.split('&')[0]
-                allusers.add(email)
+                if 'index_1.m3u8' in line:
+                    email = email_regex.search(line).group()
+                    email = email.split(',')[0]
+                    email = email.replace('"args":"', '')
+                    email = email.strip('"')
+                    email = email.split('&')[0]
+                    allusers.add(email)
+                    if email in counter:
+                        counter[email] += 1
+                    else:
+                        counter[email] = 1
             except:
                 pass
 
-print(len(allusers))
-
 for user in allusers:
-    print(user)
+    print(user, counter[user]/10)
