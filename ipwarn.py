2138bcf05fb44f4d65d7c42abb2b474e85151be0
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-06-04 02:06:02 +0800

    Revert "Rate limit"
    This reverts commit db4188267f037f9c90047a062d4240d1435f30ef.

--- ipwarn.py
+++ ipwarn.py
@@ -55,7 +55,6 @@ for id in iplist:
                     x_type = 'Mobile'
             x_full = '{} - {}'.format(x_isp, x_type).strip(' -')
             compose += addr + ' ({})\n'.format(x_full)
-            time.sleep(1)
 
 sender = compose.split('\n')
 linecounter = 0
