efbb8c00f66c53ea750f1d05fa96124321d5838a
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2020-12-16 23:47:42 +0800

    Create 9aupdate.py

---
+++ 9aupdate.py
@@ -0,0 +1,26 @@
+import telegram
+import schedule
+import secrets
+import time
+from secret import group, bottoken, link
+import os
+
+bot = telegram.Bot(token=bottoken)
+
+
+def changecode():
+    bot.send_message(chat_id=group, text="_Changing password for 9a..._",
+                     parse_mode=telegram.ParseMode.MARKDOWN)
+    code = secrets.token_hex(3).upper()
+    olddir = "/var/www/html/" + os.listdir("/var/www/html")[0]
+    newdir = "/var/www/html/" + code
+    os.rename(olddir, newdir)
+    bot.send_message(chat_id=group, text=f"{link}{code}/")
+
+
+changecode()
+schedule.every().friday.at("00:00").do(changecode)
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
