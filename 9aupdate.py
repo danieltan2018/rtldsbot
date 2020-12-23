a611947182fd9fe48847761177cc176680fbc7fc
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2020-12-24 02:42:59 +0800

    Patch listdir

--- 9aupdate.py
+++ 9aupdate.py
@@ -11,8 +11,14 @@ bot = telegram.Bot(token=bottoken)
 def changecode():
     bot.send_message(chat_id=group, text="_Changing code for 9a..._",
                      parse_mode=telegram.ParseMode.MARKDOWN)
-    code = secrets.token_hex(3).upper()
-    olddir = "/var/www/html/" + os.listdir("/var/www/html")[0]
+    code = '0'
+    while '0' in code or '1' in code:
+        code = secrets.token_hex(4).upper()
+    p = os.listdir("/var/www/html")
+    for i in p:
+        if os.path.isdir(i):
+            olddir = "/var/www/html/" + i
+            break
     newdir = "/var/www/html/" + code
     os.rename(olddir, newdir)
     bot.send_message(chat_id=group, text=f"{link}{code}/")
